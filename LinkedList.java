ee3ad6a03ed31cca5da6445b5904bbded8b7558d
1U23IT021-Dharani <142375556+1U23IT021-Dharani@users.noreply.github.com>
2025-01-31 04:33:19 +0000

    done8

--- LinkedList.java
+++ LinkedList.java
@@ -62,10 +62,19 @@ int data,data1;
         }
     }
       public void insertEnd(int data) 
-    {    
-       //TYPE YOUR CODE HERE 
-    }  
-     
+    { 
+      Node newNode = new Node(data);
+      if(head == null)
+      {
+        head = newNode;
+        tail = newNode;
+      }   
+      else{
+        tail.next = newNode;
+        tail = newNode;
+      }
+      System.out.println("Element Inserted at the End");      
+    }       
 public void displayList() {
         Node current = head;
         while (current != null) {
